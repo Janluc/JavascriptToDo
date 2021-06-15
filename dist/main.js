867718fa4fcb02459c056b92a13381a538b5df07
Janluc <Janlucsaneaux@gmail.com>
2021-06-15 15:56:56 -0400

    Initial commit

---
+++ dist/main.js
@@ -0,0 +1,9 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+window.alert("Linked JS and webpack")
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9qYXZhc2NyaXB0LXRvLWRvLy4vc3JjL2luZGV4LmpzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7Ozs7O0FBQUEscUMiLCJmaWxlIjoibWFpbi5qcyIsInNvdXJjZXNDb250ZW50IjpbIndpbmRvdy5hbGVydChcIkxpbmtlZCBKUyBhbmQgd2VicGFja1wiKSJdLCJzb3VyY2VSb290IjoiIn0=
