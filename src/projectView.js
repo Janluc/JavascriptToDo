996220385c7962148c0047121039d7764cb8f355
Janluc <Janlucsaneaux@gmail.com>
2021-06-15 20:41:11 -0400

    Add functionality to create a To Do item

--- src/projectView.js
+++ src/projectView.js
@@ -1,13 +1,16 @@
 import Project from './projectClass';
 import ToDoItem from './toDoClass';
 
-
 let createNav = (project) => {
   let main = document.querySelector("#content");
   let test = document.createElement('h1')
   test.textContent = project.projectName;
+
+  let btn = document.createElement('button');
+  btn.textContent = "+"
+  btn.addEventListener('click', () => newItemWindow(project));
   
-  main.append(test)
+  main.append(test, btn)
 
 }
 let createContent = (project) => {
@@ -27,7 +30,63 @@ let createContent = (project) => {
     div.append(title, description, dueDate, priority);
     main.append(div);
   });
-  
+}
+
+let newItemWindow = (project) => {
+  let main = document.querySelector("#content");
+  let div = document.createElement('div');
+  div.classList.add('modal')
+
+  let contentDiv = document.createElement('div');
+  let form = document.createElement('form');
+  let title = document.createElement('input');
+  title.setAttribute('id', 'title');
+  title.setAttribute('type', 'text');
+  title.setAttribute('placeholder', 'Title');
+
+  let description = document.createElement('input');
+  description.setAttribute('id', 'description');
+  description.setAttribute('type', 'text');
+  description.setAttribute('placeholder', 'Description');
+
+  let dueDate = document.createElement('input');
+  dueDate.setAttribute('id', 'dueDate');
+  dueDate.setAttribute('type', 'date');
+
+  let priority = document.createElement('input');
+  priority.setAttribute('id', 'priority');
+  priority.setAttribute('type', 'text');
+  priority.setAttribute('placeholder', 'Priority')
+
+  let submitForm = document.createElement('button')
+  submitForm.textContent = "Create To Do";
+  submitForm.addEventListener('click', () => submitNewItem(project))
+
+  form.append(title, description, dueDate, priority, submitForm)
+  contentDiv.append(form);
+  div.append(contentDiv);
+
+  contentDiv.classList.add('modal-content')
+  main.append(div);
+}
+
+let submitNewItem = (project) => {
+  let title = document.querySelector('#title').value;
+  let description = document.querySelector('#description').value;
+  let dueDate = document.querySelector('#dueDate').value;
+  let priority = document.querySelector('#priority').value;
+
+  project.toDoList.push(new ToDoItem(title, description, dueDate, priority));
+  refreshProject(project);
+}
+
+let refreshProject = (project) => {
+  let main = document.querySelector("#content");
+  while (main.firstChild) {
+    main.removeChild(main.firstChild);
+  }
+  createNav(project);
+  createContent(project);
 }
 
 export default {createNav, createContent};
